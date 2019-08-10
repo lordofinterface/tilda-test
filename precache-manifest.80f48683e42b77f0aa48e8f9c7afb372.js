ab37c615c53102a0751f0143f79ceb8f00fc4eb1
Zakharov Vladislav <zakharovvlad.o@gmail.com>
2019-08-10 18:48:26 +0300

    Updates

---
+++ precache-manifest.80f48683e42b77f0aa48e8f9c7afb372.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92914861373e66769221db590f0adf93",
+    "url": "/tilda-test/index.html"
+  },
+  {
+    "revision": "cb546e7f6f5a10742a65",
+    "url": "/tilda-test/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7a1448c5eea4d171e2ab",
+    "url": "/tilda-test/static/css/main.09159c81.chunk.css"
+  },
+  {
+    "revision": "cb546e7f6f5a10742a65",
+    "url": "/tilda-test/static/js/2.35ee83cb.chunk.js"
+  },
+  {
+    "revision": "7a1448c5eea4d171e2ab",
+    "url": "/tilda-test/static/js/main.f83d74f5.chunk.js"
+  },
+  {
+    "revision": "9544df9f55f92d75a556",
+    "url": "/tilda-test/static/js/runtime~main.a673f031.js"
+  },
+  {
+    "revision": "708809d8bb1e7aad6cb7c0ca9afeb8c3",
+    "url": "/tilda-test/static/media/arrow.708809d8.svg"
+  },
+  {
+    "revision": "932933653c14e0bb2b4fdf4a15846a9d",
+    "url": "/tilda-test/static/media/close.93293365.svg"
+  },
+  {
+    "revision": "e52b22a72ac7eec44f64a3c742e31b08",
+    "url": "/tilda-test/static/media/eastwood-done.e52b22a7.png"
+  }
+]);
